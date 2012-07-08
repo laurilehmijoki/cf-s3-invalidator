d6ca3b00293b24bf795c2ced0fdb4ea900ad8d02
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2012-07-08 18:02:52 +0300

    add namespace CloudfrontS3Invalidator

--- lib/s3_loader.rb
+++ lib/s3_loader.rb
@@ -1,18 +1,20 @@
 require 'rubygems'
 require 'aws-sdk'
 
-class S3Loader
-  def initialize(key, secret)
-    @s3 = AWS::S3.new(
-      :access_key_id => key,
-      :secret_access_key => secret)
-  end
-
-  def list_keys(bucket_name)
-    s3_objects = []
-    @s3.buckets[bucket_name].objects.each do |object|
-      s3_objects << object.key
+module CloudfrontS3Invalidator
+  class S3Loader
+    def initialize(key, secret)
+      @s3 = AWS::S3.new(
+        :access_key_id => key,
+        :secret_access_key => secret)
     end
-    s3_objects
+
+    def list_keys(bucket_name)
+      s3_objects = []
+      @s3.buckets[bucket_name].objects.each do |object|
+        s3_objects << object.key
+      end
+      s3_objects
+    end
   end
 end
